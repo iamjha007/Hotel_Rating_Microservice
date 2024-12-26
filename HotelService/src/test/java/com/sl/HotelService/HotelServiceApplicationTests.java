e34087c5c26e0d324a23cd59155e6ff9765d7f7d
iamjha007 <iamjha007@gmail.com>
2024-12-27 00:46:17 +0530

    Hotel Service project intilized

---
+++ HotelService/src/test/java/com/sl/HotelService/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl.HotelService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
