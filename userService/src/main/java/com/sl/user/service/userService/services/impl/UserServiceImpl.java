1d28fb22c23a4e642fc3799b9de3872b690499ea
iamjha007 <iamjha007@gmail.com>
2024-12-27 20:56:43 +0530

    restTemplate call to rating service implemented

--- userService/src/main/java/com/sl/user/service/userService/services/impl/UserServiceImpl.java
+++ userService/src/main/java/com/sl/user/service/userService/services/impl/UserServiceImpl.java
@@ -1,21 +1,29 @@
 package com.sl.user.service.userService.services.impl;
 
 import com.sl.user.service.userService.Repositories.UserRepository;
+import com.sl.user.service.userService.entity.Rating;
 import com.sl.user.service.userService.entity.User;
 import com.sl.user.service.userService.exceptions.NoUserFoundException;
 import com.sl.user.service.userService.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
     @Override
     public User saveUser(User user) {
 
@@ -24,12 +32,26 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUser() {
-        return userRepository.findAll();
+
+        List<User> users = userRepository.findAll();
+
+        for(User user : users){
+            ResponseEntity<Rating[]> ratingList = restTemplate.getForEntity("http://localhost:8083/ratings/users/"+user.getId(), Rating[].class);
+            log.info("{}",ratingList.getBody());
+            user.setRatings(List.of(ratingList.getBody()));
+        }
+        return users;
     }
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()->new NoUserFoundException());
+
+        User user = userRepository.findById(userId).orElseThrow(()->new NoUserFoundException());
+
+        ResponseEntity<Rating[]> ratingList = restTemplate.getForEntity("http://localhost:8083/ratings/users/"+userId, Rating[].class);
+        log.info("{}",ratingList.getBody());
+        user.setRatings(List.of(ratingList.getBody()));
+        return user;
     }
 
     @Override
